3c110cf2661fa195b5e73dcf0862cb548e7a81a5
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-11-18 13:24:39 -0300

    chore: first commit base project

---
+++ app/src/main/java/aguiar/fedechen/flavio/mobible/MainActivity.java
@@ -0,0 +1,14 @@
+package aguiar.fedechen.flavio.mobible;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
