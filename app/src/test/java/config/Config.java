0f6a0505d55ed851db956e083992ca9e4ec24f0a
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-11-19 16:53:49 -0300

    feat: create class config

---
+++ app/src/test/java/config/Config.java
@@ -0,0 +1,6 @@
+package config;
+
+public class Config {
+    public static final String API_URL = "https://www.abibliadigital.com.br/api/";
+    public static final String ACCESS_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlR1ZSBOb3YgMjIgMjAyMiAxNjowODo1NyBHTVQrMDAwMC50ZXN0ZUBneW91bC5jb20iLCJpYXQiOjE2NjkxMzMzMzd9.mYevG8nt2kwAH7QA7h7wcyZSknm3Mmy-QX2f12AWDss";
+}
