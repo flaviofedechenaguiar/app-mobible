a94a6d111005d1bc153091bbbd94e2dbdc37099c
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-11-26 20:14:43 -0300

    feat: create VersesActivity class

---
+++ app/src/main/java/aguiar/fedechen/flavio/mobible/VersesActivity.java
@@ -0,0 +1,14 @@
+package aguiar.fedechen.flavio.mobible;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VersesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verses);
+    }
+}
